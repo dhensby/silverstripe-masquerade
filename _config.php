9cc7b7bb512ce51b8497e7f387c7d330bd962954
Daniel Hensby <dhensby@silverstripe.com>
2016-02-04 16:29:47 +0000

    Adding tests, changelog and turning off user last visit

--- _config.php
+++ _config.php
@@ -1,1 +1,6 @@
 <?php
+
+//turn off member tracking when masquerading
+if (Session::get('Masquerade.Old.loggedInAs')) {
+    Member::config()->log_last_visited = false;
+}
---
+++ tests/MasqueradeMemberTest.php
@@ -0,0 +1,42 @@
+<?php
+
+class MasqueradeMemberTest extends SapphireTest {
+
+    protected static $fixture_file = 'MasqueradeMemberTest.yml';
+
+    public function testCanMasquerade()
+    {
+        $this->logInWithPermission('ADMIN');
+        $admin = Member::currentUser();
+
+        $member = $this->objFromFixture('Member', 'user');
+
+        //added function correctly
+        $this->assertTrue($member->hasMethod('canMasquerade'));
+
+        // admin can masquerade as another
+        $this->assertTrue($member->canMasquerade());
+
+        //admin can't masquerade as themselves
+        $this->assertFalse($admin->canMasquerade());
+
+        $admin->logOut();
+
+        // no logged in user can't masquerade
+        $this->assertFalse($member->canMasquerade());
+
+        //admin can masquerade
+        $this->assertTrue($member->canMasquerade($admin));
+
+        // member cannot masquerade as themeselves
+        $this->assertFalse($member->canMasquerade($member));
+
+        $member->logIn();
+        // member can't masquerade as themselves
+        $this->assertFalse($member->canMasquerade());
+
+        // member can't masquerade as an admin
+        $this->assertFalse($admin->canMasquerade());
+    }
+
+}
