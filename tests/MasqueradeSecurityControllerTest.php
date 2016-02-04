267856b4e6cb5c8994f573b2e067d799a05208da
Daniel Hensby <dhensby@silverstripe.com>
2016-02-04 17:04:19 +0000

    Adding SecurityController test

---
+++ tests/MasqueradeSecurityControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class MasqueradeSecurityControllerTest extends SapphireTest {
+
+    protected static $fixture_file = 'MasqueradeMemberTest.yml';
+
+    public function testLogout()
+    {
+        $this->logInWithPermission('ADMIN');
+        $admin = Member::currentUser();
+        $member = $this->objFromFixture('Member', 'user');
+
+        $member->masquerade();
+
+        $this->assertEquals($member->ID, Session::get('loggedInAs'));
+        $sc = new MasqueradeSecurityController();
+        $sc->init();
+        $sc->logout(false);
+
+        $this->assertEquals($admin->ID, Session::get('loggedInAs'));
+
+    }
+
+}
